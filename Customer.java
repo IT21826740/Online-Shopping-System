6eb6dee361fc845af12d9f9061396dc15df2bfbb
K.D.Dilini <111214065+IT21826740@users.noreply.github.com>
2024-08-07 20:26:06 +0530

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,34 @@
+package Q3;
+
+public class Customer {
+   private int customerID;
+    private String name;
+    private String email;
+
+   public Customer(int customerID,String name, String email){
+        this.customerID=customerID;
+        this.name=name;
+        this.email=email;
+
+    }
+    
+    public int getCustomerID() {
+        return customerID;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
